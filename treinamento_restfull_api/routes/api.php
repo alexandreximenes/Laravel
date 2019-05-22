1e5bd17de7d1c4d0a69de89ae0ceca3e964cd20e
Alexandre Ximenes <xyymenes@gmail.com>
2019-05-21 23:02:18 -0300

    implements methods with plus of one relationships

--- treinamento_restfull_api/routes/api.php
+++ treinamento_restfull_api/routes/api.php
@@ -24,6 +24,8 @@
 Route::resource('buyers', "Buyer\BuyerController", ['only' => ['index', 'show']]);
 Route::resource('categories', "Category\CategoryController", ['except' => ['create', 'edit']]);
 Route::resource('sellers', "Seller\SellerController", ['only' => ['index', 'show']]);
-Route::resource('transactions', "Transaction\TransactionController");
+Route::resource('transactions', "Transaction\TransactionController", ['only' => ['index', 'show']]);
+Route::resource('transactions.categories', "Transaction\TransactionCategoryController", ['only' => ['index']]);
+Route::resource('transactions.sellers', "Transaction\TransactionSellerController", ['only' => ['index']]);
 Route::resource('users', "User\UserController", ['except' => ['create', 'edit']]);
 Route::resource('product', "Product\ProductController", ['only' => ['index', 'show']]);
