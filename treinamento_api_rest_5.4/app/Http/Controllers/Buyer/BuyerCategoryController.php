0a4ae5e457a0e383476cffc51218fb6cfe903a9c
Alexandre Ximenes <xyymenes@gmail.com>
2019-06-24 23:32:02 -0300

    implments controllers, methods

---
+++ treinamento_api_rest_5.4/app/Http/Controllers/Buyer/BuyerCategoryController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\Buyer;
+
+use App\Buyer;
+use App\Http\Controllers\ApiController;
+
+class BuyerCategoryController extends ApiController
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @param Buyer $buyer
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Buyer $buyer)
+    {
+        $sellers = $buyer->transactions()
+            ->with('product.categories')
+            ->get()
+            ->pluck('product.categories')
+            ->collapse()
+            ->unique('id')
+            ->values();
+
+        return $this->showAll($sellers);
+
+    }
+}
---
+++ treinamento_api_rest_5.4/app/Http/Controllers/Category/CategorySellerController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers\Category;
+
+use App\Category;
+use App\Http\Controllers\ApiController;
+
+class CategorySellerController extends ApiController
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @param Category $category
+     * @return void
+     */
+    public function index(Category $category)
+    {
+        $sellers = $category->products()
+            ->with('seller')
+            ->get()
+            ->pluck('seller')
+            ->unique()
+            ->values();
+
+        return $this->showAll($sellers);
+    }
+}
