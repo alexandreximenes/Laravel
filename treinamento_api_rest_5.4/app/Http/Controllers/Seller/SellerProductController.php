978777699d3886987635cdb4e4375418f11b9608
Alexandre Ximenes <alexandre.ximenes@livetouch.com.br>
2019-07-02 13:26:37 -0300

    implements update sellerProduct

--- treinamento_api_rest_5.4/app/Http/Controllers/Seller/SellerProductController.php
+++ treinamento_api_rest_5.4/app/Http/Controllers/Seller/SellerProductController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\ApiController;
 use App\Product;
 use App\Seller;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 
 class SellerProductController extends ApiController
 {
@@ -55,13 +56,43 @@ class SellerProductController extends ApiController
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Seller  $seller
-     * @return \Illuminate\Http\Response
+     * @param \Illuminate\Http\Request $request
+     * @param \App\Seller $seller
+     * @param Product $product
+     * @return void
      */
-    public function update(Request $request, Seller $seller)
+    public function update(Request $request, Seller $seller, Product $product)
     {
-        //
+        $rules = [
+            'quantity' => 'integer|min:1',
+            'status' => 'in:' . Product::UNAVAILABLE_PRODUTO . ',' . Product::UNAVAILABLE_PRODUTO,
+            'image' => 'image'
+        ];
+
+        $this->validate($request, $rules);
+
+        $this->checkSeller($seller, $product);
+
+        $product->fill($request->intersect([
+            'name',
+            'description',
+            'quantity'
+        ]));
+
+        if($request->has('status')){
+            $product->status = $request->status;
+            if($product->isAvailable() && $product->categories()->count() === 0){
+                return $this->errorResponse('An active product must have at least one category', 409);
+            }
+        }
+
+        if($product->isClean()){
+            return $this->errorResponse('you need to specify a different value to update', 422);
+        }
+
+        $product->save();
+
+        return $this->showOne($product);
     }
 
     /**
@@ -73,5 +104,12 @@ class SellerProductController extends ApiController
     public function destroy(Seller $seller)
     {
         //
+    }
+
+    private function checkSeller(Seller $seller, Product $product)
+    {
+        if($seller->id !== $product->seller_id){
+            throw new HttpException(422, 'The specified seller is not the actual seller of the product');
+        }
     }
 }
